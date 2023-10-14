f8a490e52ac0ebee85fe18405e4aaf937ffeca39
Long Nguyen <me@long-nguyen.dev>
2023-10-13 19:24:55 -0500

    Repo: Prepare StoneworkReact for publishing

---
+++ packages/react/src/index.ts
@@ -0,0 +1,1 @@
+export { ColumnStonework } from "./components";
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { defineConfig } from "vitest/config";
 
 // Typescript Configuration
@@ -8,15 +9,36 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
 
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "stoneworkReact",
+      // the proper extensions will be added
+      fileName: "stoneworkReact",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "JSX",
+        },
+      },
+    },
+  },
   plugins: [
     tsconfigPaths(),
     react(),
+    cssInjectedByJsPlugin(),
     dts({
       include: ["src"],
-      exclude: ["test", "**/__test__/**/*"],
-      rollupTypes: true,
+      exclude: ["test", "**/__test__/**/*", "src/app.tsx", "src/site.tsx"],
     }),
   ],
   test: {},
